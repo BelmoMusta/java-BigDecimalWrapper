b4fdb8e65d37a663f36e6404ce5104e6242336b5
Mustapha Belmokhtar <mbelmokhtar@sqli.com>
2022-04-09 23:42:42 +0000

    finalize

--- src/main/java/org/mustabelmo/bigdecimalwrapper/BigDecimalWrapper.java
+++ src/main/java/org/mustabelmo/bigdecimalwrapper/BigDecimalWrapper.java
@@ -2,93 +2,89 @@ package org.mustabelmo.bigdecimalwrapper;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Objects;
 
-public class BigDecimalWrapper extends Number {
-	private final BigDecimal innerDecimal;
-	
-	public BigDecimalWrapper(BigDecimal innerDecimal) {
-		this.innerDecimal = innerDecimal;
-	}
-	
-	@Override
-	public int intValue() {
-		return innerDecimal.intValue();
-	}
-	
-	@Override
-	public long longValue() {
-		return innerDecimal.longValue();
-	}
-	
-	@Override
-	public float floatValue() {
-		return innerDecimal.floatValue();
-	}
-	
-	@Override
-	public double doubleValue() {
-		return innerDecimal.doubleValue();
-	}
-	
-	public static BigDecimal valueOf(long val) {return BigDecimal.valueOf(val);}
-	
-	public static BigDecimal valueOf(double val) {return BigDecimal.valueOf(val);}
-	
-	public BigDecimal add(BigDecimal augend) {return innerDecimal.add(augend);}
-	
-	public BigDecimal add(BigDecimal augend, MathContext mc) {return innerDecimal.add(augend, mc);}
-	
-	public BigDecimal subtract(BigDecimal subtrahend) {return innerDecimal.subtract(subtrahend);}
-	
-	public BigDecimal subtract(BigDecimal subtrahend, MathContext mc) {return innerDecimal.subtract(subtrahend, mc);}
-	
-	public BigDecimal divide(BigDecimal divisor, int scale, RoundingMode roundingMode) {return innerDecimal.divide(divisor, scale, roundingMode);}
-	
-	public BigDecimal divide(BigDecimal divisor, RoundingMode roundingMode) {return innerDecimal.divide(divisor,
-			roundingMode);}
-	
-	public BigDecimal divide(BigDecimal divisor) {return innerDecimal.divide(divisor);}
-	
-	public BigDecimal divide(BigDecimal divisor, MathContext mc) {return innerDecimal.divide(divisor, mc);}
-	
-	public BigDecimal[] divideAndRemainder(BigDecimal divisor) {return innerDecimal.divideAndRemainder(divisor);}
-	
-	public BigDecimal[] divideAndRemainder(BigDecimal divisor, MathContext mc) {return innerDecimal.divideAndRemainder(divisor, mc);}
-	
-	public BigDecimal sqrt(MathContext mc) {return innerDecimal.sqrt(mc);}
-	
-	public BigDecimal pow(int n) {return innerDecimal.pow(n);}
-	
-	public BigDecimal abs(MathContext mc) {return innerDecimal.abs(mc);}
-	
-	public BigDecimal negate() {return innerDecimal.negate();}
-	
-	public int compareTo(BigDecimal val) {return innerDecimal.compareTo(val);}
-	
-	@Override
-	public String toString() {return innerDecimal.toString();}
-	
-	public String toPlainString() {return innerDecimal.toPlainString();}
-	
-	public BigInteger toBigInteger() {return innerDecimal.toBigInteger();}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		BigDecimalWrapper that = (BigDecimalWrapper) o;
-		return innerDecimal.compareTo(that.innerDecimal) == 0;
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(innerDecimal);
-	}
+import static java.math.BigDecimal.ROUND_UNNECESSARY;
+
+public class BigDecimalWrapper {
+    private final BigDecimal innerDecimal;
+
+    public BigDecimalWrapper() {
+        this(BigInteger.ZERO);
+    }
+
+    public BigDecimalWrapper(BigDecimal innerDecimal) {
+        this.innerDecimal = Objects.requireNonNull(innerDecimal);
+    }
+
+    public BigDecimalWrapper(int i) {
+        this(BigDecimal.valueOf(i));
+    }
+
+    public BigDecimalWrapper(long i) {
+        this(BigDecimal.valueOf(i));
+    }
+
+    public BigDecimalWrapper(double i) {
+        this(BigDecimal.valueOf(i));
+    }
+
+    public BigDecimalWrapper(String i) {
+        this(new BigDecimal(i));
+    }
+
+    public BigDecimalWrapper(BigInteger i) {
+        this(BigDecimal.valueOf(i.intValue()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BigDecimalWrapper that = (BigDecimalWrapper) o;
+        return innerDecimal.compareTo(that.innerDecimal) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(innerDecimal);
+    }
+
+    @Override
+    public String toString() {
+        return innerDecimal.toString();
+    }
+
+    public String toCurrencyFormat(Locale locale) {
+        return NumberFormat.getCurrencyInstance(locale).format(innerDecimal);
+    }
+
+    public String toCurrencyFormat() {
+        return toCurrencyFormat(Locale.getDefault());
+    }
+
+    public String format(String format) {
+        return format(format, Locale.getDefault());
+    }
+
+    public String format(String format, Locale locale) {
+        DecimalFormat df = new DecimalFormat(format, new DecimalFormatSymbols(locale));
+        return df.format(innerDecimal);
+    }
+
+    public BigDecimalWrapper setScale(int scale) {
+        return setScale(scale, ROUND_UNNECESSARY);
+    }
+
+    public BigDecimalWrapper setScale(int scale, int roundingMode) {
+        return new BigDecimalWrapper(innerDecimal.setScale(scale, roundingMode));
+    }
 }
--- src/test/java/org/mustabelmo/bigdecimalwrapper/tests/BigDecimalWrapperTest.java
+++ src/test/java/org/mustabelmo/bigdecimalwrapper/tests/BigDecimalWrapperTest.java
@@ -1,10 +1,118 @@
 package org.mustabelmo.bigdecimalwrapper.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.mustabelmo.bigdecimalwrapper.BigDecimalWrapper;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Objects;
 
 public class BigDecimalWrapperTest {
     @Test
-    public void testWrappedBigDecimal() {
-    
+    public void testEquality() {
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(112.33);
+        BigDecimalWrapper wrapper2 = new BigDecimalWrapper(112.330);
+        Assert.assertEquals(wrapper, wrapper2);
+    }
+
+    @Test
+    public void testEqualityTautology() {
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(112.33);
+        Assert.assertEquals(wrapper, wrapper);
+    }
+
+    @Test
+    public void testEqualityOtherInstance() {
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(112.33);
+        Assert.assertNotEquals(wrapper, "wrapper");
+    }
+
+    @Test
+    public void testEqualityIfNull() {
+        BigDecimalWrapper wrapper = null;
+        BigDecimalWrapper wrapper2 = new BigDecimalWrapper(112.330);
+        Assert.assertNotEquals(wrapper, wrapper2);
+    }
+
+    @Test
+    public void testEqualityAsStrings() {
+        BigDecimalWrapper zeroString = new BigDecimalWrapper("0");
+        BigDecimalWrapper oldZero = new BigDecimalWrapper(BigDecimal.ZERO);
+        Assert.assertEquals(zeroString, oldZero);
+    }
+
+
+    @Test
+    public void testHashCode() {
+        BigDecimal bigDecimal = new BigDecimal(114);
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(bigDecimal);
+        Assert.assertEquals(Objects.hash(bigDecimal), wrapper.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "55.6";
+        BigDecimalWrapper wrapper = new BigDecimalWrapper("55.6");
+        Assert.assertEquals(expected, wrapper.toString());
+    }
+
+    @Test
+    public void testToCurrencyFormat() {
+        BigDecimal bigDecimal = new BigDecimal("33.49");
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(bigDecimal);
+        String expected = NumberFormat.getCurrencyInstance().format(bigDecimal);
+        Assert.assertEquals(expected, wrapper.toCurrencyFormat());
+    }
+
+    @Test
+    public void testToCurrencyFormatWithLocale() {
+        BigDecimalWrapper wrapper = new BigDecimalWrapper("33.49");
+        Assert.assertEquals("$33.49", wrapper.toCurrencyFormat(Locale.US));
+    }
+
+    @Test
+    public void format() {
+        String numberAsString = "123456.75";
+        BigDecimalWrapper wrapper = new BigDecimalWrapper(numberAsString);
+        String format = "#,###.00";
+        DecimalFormat df = new DecimalFormat(format); // locale default
+        Assert.assertEquals(df.format(new BigDecimal(numberAsString)), wrapper.format(format));
+    }
+
+    @Test
+    public void setScale() {
+        BigDecimalWrapper bigDecimalWrapper = new BigDecimalWrapper(45.230).setScale(2);
+        BigDecimalWrapper bigDecimalWrapperScaled = new BigDecimalWrapper(45.23);
+        Assert.assertEquals(bigDecimalWrapper, bigDecimalWrapperScaled);
+    }
+
+    @Test
+    public void testSetScale() {
+        BigDecimalWrapper bigDecimalWrapper = new BigDecimalWrapper(454.23445).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimalWrapper bigDecimalWrapperScaled = new BigDecimalWrapper(454.23);
+        Assert.assertEquals(bigDecimalWrapper, bigDecimalWrapperScaled);
+    }
+
+    @Test
+    public void testInt() {
+        int y = 11;
+        BigDecimalWrapper bigDecimalWrapper = new BigDecimalWrapper(y);
+        Assert.assertEquals("11", bigDecimalWrapper.toString());
+    }
+
+    @Test
+    public void testLong() {
+        long y = 12L;
+        BigDecimalWrapper bigDecimalWrapper = new BigDecimalWrapper(y);
+        Assert.assertEquals("12", bigDecimalWrapper.toString());
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        BigDecimalWrapper bigDecimalWrapper = new BigDecimalWrapper();
+        Assert.assertEquals("0", bigDecimalWrapper.toString());
     }
 }
